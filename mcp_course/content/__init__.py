062123072b1deda8810f15502b134b46e32d9cc9
vincent porte <vincent@neuralia.co>
2025-10-16 10:02:58 +0200

    remove interactive course & cli, cause it is out of scope

--- mcp_course/content/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Course content management and loading utilities."""
-
-from mcp_course.content.loader import FileSystemContentLoader
-from mcp_course.content.navigator import CourseNavigator
-
-
-__all__ = ["CourseNavigator", "FileSystemContentLoader"]
